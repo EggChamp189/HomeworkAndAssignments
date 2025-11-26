76568791394d0596193e7c4d0c87e056e1f918ac
Tetra <tetraacgee@gmail.com>
2025-11-26 16:09:58 -0500

    Assignment 12 done!
    aaaaandd thats a wrap on the assignments!

--- assignment12/src/PriorityQueue.h
+++ assignment12/src/PriorityQueue.h
@@ -59,6 +59,7 @@ namespace csi281 {
     // NOTE: Our heap starts at 0, not 1
     T peek() {
       // YOUR CODE HERE
+      return heap[0];
     }
 
     // Remove the next element (max element) in the heap and return it
@@ -69,6 +70,13 @@ namespace csi281 {
     // after a pop.
     T pop() {
       // YOUR CODE HERE
+      T max = heap[0];
+      heap[0] = heap[heapSize - 1];
+      heapSize--;
+
+      maxHeapify(0);
+
+      return max;
     }
 
     // Put a new element into the priority queue
@@ -81,6 +89,15 @@ namespace csi281 {
     // the end of the vector heap
     void push(T key) {
       // YOUR CODE HERE
+      heap.push_back(key);
+
+      int i = heapSize;
+      heapSize++;
+
+      while (i > 0 && heap[parent(i)] < heap[i]) {
+        std::swap(heap[i], heap[parent(i)]);
+        i = parent(i);
+      }
     }
 
     // How many items are in the priority queue?
@@ -100,6 +117,19 @@ namespace csi281 {
     // NOTE: Macros left() and right() are defined at the top of this file
     void maxHeapify(int i) {
       // YOUR CODE HERE
+      int largest = i; // so always overwritten
+
+      int l = left(i);
+      int r = right(i);
+
+      if (l < heapSize && heap[l] > heap[largest]) largest = l;
+      if (r < heapSize && heap[r] > heap[largest]) largest = r;
+      // if the index i is already the largest, then the subtree is already a max heap and terminates.
+      if (largest != i) {
+        std::swap(heap[i], heap[largest]);
+        maxHeapify(largest);
+      }
+
     }
 
     vector<T> heap;
